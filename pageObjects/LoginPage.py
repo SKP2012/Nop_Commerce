00227346f6388cf48c4569f716052dfb298fe8c9
SKP2012 <2012shubhampawar@gmail.com>
2025-07-02 21:04:32 +0530

    Tevdach Code

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 
 class LoginPage:
     textbox_username_id = "Email"
-
     textbox_password_id = "Password"
     button_login_xpath = "//button[normalize-space()='Log in']"
     link_logout_linktext = "Logout"
@@ -27,7 +26,9 @@ class LoginPage:
         self.driver.find_element(By.ID,self.textbox_password_id).send_keys("password")
 
     def clickLogin(self):
+
         self.driver.find_element(By.XPATH,self.button_login_xpath).click()
 
     def clickLogout(self):
+
         self.driver.find_element(By.LINK_TEXT,self.link_logout_linktext).click()
